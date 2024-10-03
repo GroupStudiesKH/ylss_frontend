df4628f74a3a1fd98fda5d47070f13a8f6fbf467
mafiapatch <g8071107@gmail.com>
2024-10-04 00:23:45 +0800

    [modify] footer initial

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,15 @@
 <template>
-    <footer class="footer bg-dark text-light">
+    <footer class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 d-lg-block d-none">
-                    <img src="/assets/img/footer_logo.png" class="mt-4">
+                <div class="col-lg-2 d-lg-block d-none">
+                    <img src="/assets/img/logo.png" class="mt-4">
                 </div>
-                <div class="col-lg-5 col-md-6 company_info col-12" >
-                    <p class="h2">{{ $t('footer.company') }}</p>
-                    <p class="h5">{{ $t('footer.intro') }}</p>
-
+                <div class="col-lg-3 col-md-6 company_info col-12" >
+                    <p class="h4">{{ $t('footer.company') }}</p>
+                    <p class="h6">{{ $t('footer.intro') }}</p>
+                </div>
+                <div class="col-lg-3">
                     <div class="address">
                         <p>{{ $t('footer.address') }}</p>
                         <p><a :href="`mailto:${$t('footer.email')}`"><span class="material-icons">&#xE0BE;</span>{{ $t('footer.email') }}</a></p>
@@ -16,11 +17,14 @@
                         <p><span class="material-icons">&#xEAD8;</span>{{ $t('footer.fax') }}</p>
                     </div>
                 </div>
-                <div class="col-lg-4 copyright col-12">
-                    <img src="/assets/img/footer_iso.png">
-                    <p>Copyright © Li Yun Enterprise Co., Ltd All rights reserved.</p>
+                <div class="col-lg-12 group_logo">
+                    <img src="/assets/img/fe_logo.png" class="mt-4">
+                    <img src="/assets/img/ac_logo.png" class="mt-4">
                 </div>
             </div>
+        </div>
+        <div class="copyright text-light">
+            <div class="container">Copyright © Li Yun Enterprise Co., Ltd All rights reserved.</div>
         </div>
     </footer>
 </template>
@@ -32,10 +36,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-.footer {
-    padding: 20px 0;
-    color: #fff;
-}
-</style>
