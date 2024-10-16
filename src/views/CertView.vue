d16722c3cf0711f92307eb1b2a3cbbc9b21ca9b3
mafiapatch <g8071107@gmail.com>
2024-10-16 10:28:46 +0800

    [modify] progress backup

--- src/views/CertView.vue
+++ src/views/CertView.vue
@@ -13,9 +13,24 @@ export default {
   },
   setup() {
     const { locale } = useI18n();
+    const selectedImage = ref(null);
+    const showModal = ref(false);
+
+    const openModal = (imageSrc) => {
+      selectedImage.value = imageSrc;
+      showModal.value = true;
+    };
+
+    const closeModal = () => {
+      showModal.value = false;
+    };
 
     return {
       locale,
+      selectedImage,
+      showModal,
+      openModal,
+      closeModal,
     };
   },
 };
@@ -47,68 +62,100 @@ export default {
 
                 <div class="col-12 mt-3" id="quality-lab">
                   <div class="row">
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-9001.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-9001.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-9001.webp">
+                        </div>
                         <p>ISO 9001  2015版(20240426-20270425)_品質管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-50001-2018.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-50001-2018.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-50001-2018.webp">
+                        </div>
                         <p>ISO 50001 2018版(20201118-20231118)_能源管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-45001-2018.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-45001-2018.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-45001-2018.webp">
+                        </div>
                         <p>ISO 45001  2018版(20220729-20250708)_職業安全衛生管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/cns-45001-en.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/cns-45001-en.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/cns-45001-en.webp">
+                        </div>
                         <p>TOSHMS   CNS 45001：2018(1110729-1140707)_台灣職業衛生管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/cns-45001-zhtw.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/cns-45001-zhtw.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/cns-45001-zhtw.webp">
+                        </div>
                         <p>TOSHMS   CNS 45001：2018(1110729-1140707)_台灣職業衛生管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-14001-2015.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-14001-2015.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-14001-2015.webp">
+                        </div>
                         <p>ISO 14001  2015版(20220729-20250721)_環境管理系統</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-14064.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-14064.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-14064.webp">
+                        </div>
                         <p>ISO 14064-1 2018版(20220810)溫室氣體查驗聲明書</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-14067-zhtw.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-14067-zhtw.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-14067-zhtw.webp">
+                        </div>
                         <p>ISO 14067 2018版(20230306-20250305)碳足跡查證聲明書</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-17025-zhtw.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-17025-zhtw.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-17025-zhtw.webp">
+                        </div>
                         <p>ISO/IEC 17025：2017；CNS17025：2018 實驗室認證證書</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iatf-16949.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iatf-16949.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iatf-16949.webp">
+                        </div>
                         <p>IATF 16949 2016版(20240426-20270425)_汽車品質管理系統驗證</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/bis-6911.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/bis-6911.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/bis-6911.webp">
+                        </div>
                         <p>BIS IS 6911</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/cns-8499.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/cns-8499.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/cns-8499.webp">
+                        </div>
                         <p>CNS 8499</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/jis.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/jis.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/jis.webp">
+                        </div>
                         <p>JQA JIS G 4305</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-17025_en.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-17025_en.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-17025_en.webp">
+                        </div>
                         <p>ISO/IEC 17025：2017；CNS17025：2018 實驗室認證證書</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-14046-zhtw.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-14046-zhtw.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-14046-zhtw.webp">
+                        </div>
                         <p>ISO 14046(2014版)-中文</p>
                     </div>
-                    <div class="col-6">
-                        <img class="w-100" src="/assets/img/cert/iso-14046-en.webp">
+                    <div class="col-6 col-md-4" @click="openModal('/assets/img/cert/iso-14046-en.webp')">
+                        <div class="clickable-image">
+                          <img class="w-100" src="/assets/img/cert/iso-14046-en.webp">
+                        </div>
                         <p>ISO 14046(2014版)-英文</p>
                     </div>
                   </div>
@@ -123,6 +170,16 @@ export default {
 
     <div id="mid_right_shape"></div>
     <div id="bottom_left_shape"></div>
+
+    <!-- Image Modal -->
+    <transition name="fade">
+      <div v-if="showModal" class="modal-overlay" @click="closeModal">
+        <div class="modal-content" @click.stop>
+          <img :src="selectedImage" alt="Enlarged image" class="modal-image">
+          <button class="close-button" @click="closeModal">&times;</button>
+        </div>
+      </div>
+    </transition>
 
   </main>
   <Footer />
