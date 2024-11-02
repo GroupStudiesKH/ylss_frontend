b0619c90ea526becee8af9b541c649a5bdb365be
mafiapatch <g8071107@gmail.com>
2024-11-02 16:08:42 +0800

    [modify] home modify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,7 +71,7 @@ export default {
           </div>
         </div>
       </div>
-      <button
+      <!-- <button
         class="carousel-control-prev"
         type="button"
         data-bs-target="#bannerCarousel"
@@ -79,8 +79,8 @@ export default {
       >
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
-      </button>
-      <button
+      </button> -->
+      <!-- <button
         class="carousel-control-next"
         type="button"
         data-bs-target="#bannerCarousel"
@@ -88,7 +88,7 @@ export default {
       >
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-      </button>
+      </button> -->
     </div>
 
     <div id="company_info">
